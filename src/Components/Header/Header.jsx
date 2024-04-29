3183636011761ef06faf34d4290788623c64ad6e
SohelShahid117 <sohelshahid09@gmail.com>
2024-04-29 10:17:25 +0600

    47-2 (Recap) Set Nested Route And Navbar For CareerHub-5min

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+//47-2 (Recap) Set Nested Route And Navbar For CareerHub-5min
+
+const Header = () => {
+  const Links = (
+    <>
+      {/* <li>
+        <a>Home</a>
+      </li>
+      <li>
+        <a>Jobs</a>
+      </li> */}
+      <li>
+        <NavLink>Home</NavLink>
+      </li>
+      <li>
+        <NavLink>Jobs</NavLink>
+      </li>
+    </>
+  );
+  return (
+    <div className="navbar bg-base-100">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </div>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            {Links}
+          </ul>
+        </div>
+        <a className="btn btn-ghost text-xl">daisyUI</a>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">{Links}</ul>
+      </div>
+      <div className="navbar-end">
+        <a className="btn">Button</a>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
--- src/Components/Root/Root.jsx
+++ src/Components/Root/Root.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Footer from "./../Footer/Footer";
+import Header from "./../Header/Header";
 //47-1 Project Setup With React Router, Tailwind And Daisy UI
 
 const Root = () => {
@@ -8,6 +9,8 @@ const Root = () => {
     <div>
       {/* <outlet></outlet> */}
       {/* <Outlettlet /> */}
+
+      <Header></Header>
       <Outlet></Outlet>
       <Footer></Footer>
     </div>
